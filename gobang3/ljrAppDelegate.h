7b90fe2667e0946eeb0ac0114d16746a42f64ad3
Bell <bell@BelltekiMacBook-Pro.local>
2013-06-20 14:48:07 +0800

    Initial Commit

---
+++ gobang3/ljrAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ljrAppDelegate.h
+//  gobang3
+//
+//  Created by Bell on 20/6/13.
+//  Copyright (c) 2013 Bell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ljrAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gobang3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gobang3
+//
+//  Created by Bell on 20/6/13.
+//  Copyright (c) 2013 Bell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ljrAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ljrAppDelegate class]));
+    }
+}
